9a9c04a87a6dd2ed7dac9f1e1241f16d74f98539
Josoé Schmidt Braga <42846760+josoebraga@users.noreply.github.com>
2020-08-15 13:03:13 -0300

    Controller produto
    Início do trabalho com o pruduto.

---
+++ app/Product.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use BinaryCats\Sku\HasSku;
+use BinaryCats\Sku\Concerns\SkuOptions;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasSku;
+    protected $table = 'product';
+    protected $primaryKey = 'id';
+
+    /**
+     * Get the options for generating the Sku.
+     *
+     * @return BinaryCats\Sku\SkuOptions
+     */
+    public function skuOptions() : SkuOptions
+    {
+        return SkuOptions::make()
+            ->forceUnique(true)
+            ->generateOnCreate(true)
+            ->refreshOnUpdate(false);
+    }
+
+}
