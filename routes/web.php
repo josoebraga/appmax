cae8a460c371b97e113955e0fa929254eb22723a
Josoé Schmidt Braga <42846760+josoebraga@users.noreply.github.com>
2020-08-16 19:30:33 -0300

    Update das Rotas com api
    Update das Rotas com api

--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,27 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+/* Rotas do Produto */
+
+Route::get('/product',['as'=>'product','uses'=>'ProductController@index']);
+Route::get('/product/data',['as'=>'productData','uses'=>'ProductController@productData']);
+Route::get('/product/create',['as'=>'productCreate','uses'=>'ProductController@create']);
+Route::post('/product/store',['as'=>'productStore','uses'=>'ProductController@store']);
+Route::get('/product/show/{id}',['as'=>'productShow','uses'=>'ProductController@show']);
+Route::post('/product/update',['as'=>'productUpdate','uses'=>'ProductController@update']);
+Route::get('/product/destroy/{id}',['as'=>'productDelete','uses'=>'ProductController@destroy']);
+
+/* Rotas de Baixar Produto */
+
+Route::get('/productSendCreateManual/{id}',['as'=>'productSendCreateManual','uses'=>'ProductSendController@create']);
+Route::post('/productSendManual',['as'=>'productSendManual','uses'=>'ProductSendController@store']);
+
+/* Rotas dos relatórios */
+
+Route::get('/product/report',['as'=>'productReport','uses'=>'ProductReportController@index']);
+
+
